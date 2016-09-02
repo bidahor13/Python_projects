408769b1cf0d71546e9b8b0c1717d1751feaf3e5
Babatunde Idahor <bidahor13@wou.edu>
2016-09-02 00:22:19 -0700

    Update rename_files.py

--- P8_wk3_Bacity/rename_files.py
+++ P8_wk3_Bacity/rename_files.py
@@ -1,11 +1,11 @@
 import os
 def rename_files():
     #(1)get file name from the  folder
-    file_list = os.listdir(r"C:\Users\Babatunde\Desktop\WOU\AAAA\Fall 2015\CS133\Python_GitHub_Files\P8_wk3_Bacity\prank")
+    file_list = os.listdir(r"C:................................Python_GitHub_Files\P8_wk3_Bacity\prank")
     #print(file_list)
     saved_path = os.getcwd()
     print("Current working directory" + saved_path)
-    os.chdir(r"C:\Users\Babatunde\Desktop\WOU\AAAA\Fall 2015\CS133\Python_GitHub_Files\P8_wk3_Bacity\prank")
+    os.chdir(r"C:..................................Python_GitHub_Files\P8_wk3_Bacity\prank")
 
     # for each file, rename filename
     for file_name in file_list:
